b96a6a69c3ee9950cb871ec5ddd65d082feb69f6
Scott Matsuda <smatsuda2014@gmail.com>
2020-01-25 04:52:02 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,10 +3,7 @@ class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, 
     :wrist_size, :glove_size, :pant_length, :pant_width
   
- def initialize(:name, :birthday, :hair_color, :eye_color)
-  @name = name
-  @birthday = birthday
-  @hair_color = hair_color
-  @eye_color = eye_color
+ def initialize(attributes)
+  attributes.each 
  end
 end
